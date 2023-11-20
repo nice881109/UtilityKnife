b0ebe042de8dcf14c1f7f3c98c3389b206a5a750
nice881109 <10742011@ntub.edu.tw>
2023-11-20 15:46:37 +0800

    Create method subtract

--- UtilityKnife/Calculator/Calculator.cs
+++ UtilityKnife/Calculator/Calculator.cs
@@ -1,10 +1,28 @@
-﻿namespace UtlityKnife.Calculator
+﻿using System.Numerics;
+
+namespace UtlityKnife.Calculator
 {
     public class Calculator
     {
         public static int Add(int x, int y)
         {
             return x + y;
+        }
+        public static Subtract(int x, int y)
+        {
+            return x - y;
+        }
+        public static int Multiply(int x, int y) 
+        {
+            return x * y;
+        }
+        public static int Division(int x, int y) 
+        {
+            return x / y;
+        }
+        public static double Division(double x, double y) 
+        {
+            return x / y;
         }
     }
 }
